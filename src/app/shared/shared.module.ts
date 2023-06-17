ed2620da0e578259b96e5a10da6968e6ece3f5f9
Oscar Alva <oscaraldrin23@gmail.com>
2023-06-17 23:34:59 +0200

    exportar un componente de un modulo a otro para lazy image done

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    LazyImageComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
-    SidebarComponent//exportamos el componente para poder usarlo en otros modulos
+    SidebarComponent,//exportamos el componente para poder usarlo en otros modulos
+    LazyImageComponent //exportamos el componente para poder usarlo en otros modulos
   ]
 })
 export class SharedModule { }
