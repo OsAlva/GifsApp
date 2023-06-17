ff6c37da8d7f5087f3a549669f6bc4ce6123671d
Oscar Alva <oscaraldrin23@gmail.com>
2023-06-18 00:05:42 +0200

    lazy loading done

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
   styleUrls: ['./lazy-image.component.css']
 })
-export class LazyImageComponent {
+export class LazyImageComponent implements OnInit{
+  
+  @Input()
+  public url!: string;
+  
+  @Input()
+  public alt: string="";
 
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if(!this.url) throw new Error('La url es requerida');
+  }
+
+  onLoad(){
+    console.log('Imagen cargada')
+    this.hasLoaded = true;
+  }
 }
